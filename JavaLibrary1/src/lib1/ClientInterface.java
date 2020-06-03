a37be619b34dfd69ab10ce07118e7825f06cf581
rex3299 <rex3299@gmail.com>
2020-06-03 23:40:59 +0530

    database utility

---
+++ JavaLibrary1/src/lib1/ClientInterface.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lib1;
+
+import java.io.ByteArrayOutputStream;
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface ClientInterface extends Remote {
+    public void doNothing() throws RemoteException;
+    public void reciveMessage(String groupName, String wow) throws RemoteException;
+    public void refresh() throws RemoteException;
+}
